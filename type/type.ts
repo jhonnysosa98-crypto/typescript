fc79ff1882ad26e84257c56727ab3690b6804291
jhonnysosa98-crypto <jhonnysosa98@gmail.com>
2026-06-10 20:09:44 -0500

    feet: type ubigeo

---
+++ type/type.ts
@@ -0,0 +1,31 @@
+
+type departament = {
+
+    id: number,
+    name: string
+
+}
+
+type province = {
+
+    id: number,
+    name: string,
+    departament_id: number
+
+}
+
+type district = {
+
+    id:number 
+    name: string
+    departament_id: number,
+    province_id: number 
+
+
+}
+
+
+
+
+
+
