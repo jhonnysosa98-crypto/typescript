591cd20c49d02e405bac3d039da045ed2bf35f60
jhonnysosa98-crypto <jhonnysosa98@gmail.com>
2026-06-10 21:03:02 -0500

    refactory interface and feed:textTransform

---
+++ src/TextTransform.ts
@@ -0,0 +1,18 @@
+
+
+
+
+import {UpperCase} from '../type/interface';
+
+export class UpperCaseStrategy implements UpperCase{
+
+    toUpperCase (name:string):string{
+        return name.toUpperCase();
+    }
+
+
+
+
+
+}
+
---
+++ type/interface.ts
@@ -0,0 +1,16 @@
+
+export interface transform {
+
+    toUpperCase(name: string):string;
+    toLowerCase(name: string):string;
+
+
+
+}
+
+
+export interface LowerCase {
+
+    toLowerCase(name: string): string;
+
+}
